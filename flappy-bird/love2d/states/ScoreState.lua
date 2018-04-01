26bbad7a20a6c936d0aed2ba17cbd65fa36fc9bb
ksmai <kasingmai@gmail.com>
2018-04-01 11:13:20 +0800

    Add different medal images to score screen

--- flappy-bird/love2d/states/ScoreState.lua
+++ flappy-bird/love2d/states/ScoreState.lua
@@ -8,6 +8,10 @@
     PlayState when they collide with a Pipe.
 ]]
 
+local gold = love.graphics.newImage('gold.png')
+local silver = love.graphics.newImage('silver.png')
+local bronze = love.graphics.newImage('bronze.png')
+
 ScoreState = Class{__includes = BaseState}
 
 --[[
@@ -31,7 +35,14 @@ function ScoreState:render()
     love.graphics.printf('Oof! You lost!', 0, 64, VIRTUAL_WIDTH, 'center')
 
     love.graphics.setFont(mediumFont)
-    love.graphics.printf('Score: ' .. tostring(self.score), 0, 100, VIRTUAL_WIDTH, 'center')
+    love.graphics.printf('Score: ' .. tostring(self.score), 0, 105, VIRTUAL_WIDTH, 'center')
+    if self.score > 3 then
+      love.graphics.draw(gold, 200, 100)
+    elseif self.score > 1 then
+      love.graphics.draw(silver, 200, 100)
+    else
+      love.graphics.draw(bronze, 200, 100)
+    end
 
     love.graphics.printf('Press Enter to Play Again!', 0, 160, VIRTUAL_WIDTH, 'center')
 end
