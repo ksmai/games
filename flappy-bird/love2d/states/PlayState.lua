eabaa3d09fb9a12cd5ce56db7f7d75db7b2ba9b2
ksmai <kasingmai@gmail.com>
2018-04-01 10:58:29 +0800

    Randomize time interval between pipe spawns

--- flappy-bird/love2d/states/PlayState.lua
+++ flappy-bird/love2d/states/PlayState.lua
@@ -25,6 +25,11 @@ function PlayState:init()
 
     -- initialize our last recorded Y value for a gap placement to base other gaps off of
     self.lastY = -PIPE_HEIGHT + math.random(80) + 20
+    self:randomizeInterval()
+end
+
+function PlayState:randomizeInterval()
+  self.timeInterval = math.random(2, 3)
 end
 
 function PlayState:update(dt)
@@ -32,7 +37,7 @@ function PlayState:update(dt)
     self.timer = self.timer + dt
 
     -- spawn a new pipe pair every second and a half
-    if self.timer > 2 then
+    if self.timer > self.timeInterval then
         -- modify the last Y coordinate we placed so pipe gaps aren't too far apart
         -- no higher than 10 pixels below the top edge of the screen,
         -- and no lower than a gap length (90 pixels) from the bottom
@@ -45,6 +50,7 @@ function PlayState:update(dt)
 
         -- reset timer
         self.timer = 0
+        self:randomizeInterval()
     end
 
     -- for every pair of pipes..
