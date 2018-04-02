1eaf98c0afd4d4edeae683150f2d4de6449a2fc6
ksmai <kasingmai@gmail.com>
2018-04-02 19:51:23 +0800

    Add a reason parameter to GameOverState

--- match3/love2d/src/states/GameOverState.lua
+++ match3/love2d/src/states/GameOverState.lua
@@ -18,6 +18,7 @@ end
 
 function GameOverState:enter(params)
     self.score = params.score 
+    self.reason = params.reason or 'GAME OVER'
 end
 
 function GameOverState:update(dt)
@@ -33,7 +34,7 @@ function GameOverState:render()
     love.graphics.rectangle('fill', VIRTUAL_WIDTH / 2 - 64, 64, 128, 136, 4)
 
     love.graphics.setColor(99, 155, 255, 255)
-    love.graphics.printf('GAME OVER', VIRTUAL_WIDTH / 2 - 64, 64, 128, 'center')
+    love.graphics.printf(self.reason, VIRTUAL_WIDTH / 2 - 64, 64, 128, 'center')
     love.graphics.setFont(gFonts['medium'])
     love.graphics.printf('Your Score: ' .. tostring(self.score), VIRTUAL_WIDTH / 2 - 64, 140, 128, 'center')
     love.graphics.printf('Press Enter', VIRTUAL_WIDTH / 2 - 64, 180, 128, 'center')
