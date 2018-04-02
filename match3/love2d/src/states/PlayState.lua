e873b71d8ac9e761b823850405fb31b689f50f26
ksmai <kasingmai@gmail.com>
2018-04-02 19:34:06 +0800

    Increment timer for each tile in a match

--- match3/love2d/src/states/PlayState.lua
+++ match3/love2d/src/states/PlayState.lua
@@ -189,6 +189,7 @@ function PlayState:calculateMatches()
         -- add score and time for each match
         for k, match in pairs(matches) do
             self.score = self.score + #match * 50
+            self.timer = math.min(60, self.timer + #match)
         end
 
         -- remove any tiles that matched from the board, making empty spaces
