4fe0f1f555b4c56ce5c6706a122ef757f8665d3a
ksmai <kasingmai@gmail.com>
2018-04-02 19:41:44 +0800

    Add shiny flag for Tile class

--- match3/love2d/src/Tile.lua
+++ match3/love2d/src/Tile.lua
@@ -13,7 +13,7 @@
 
 Tile = Class{}
 
-function Tile:init(x, y, color, variety)
+function Tile:init(x, y, color, variety, shiny)
     -- board positions
     self.gridX = x
     self.gridY = y
@@ -25,10 +25,29 @@ function Tile:init(x, y, color, variety)
     -- tile appearance/points
     self.color = color
     self.variety = variety
+    self.shiny = shiny
+    if self.shiny then
+      self.shinyAlpha = 0
+      self.shinyIncreasing = true
+    end
 end
 
 function Tile:update(dt)
-
+  if self.shiny then
+    if self.shinyIncreasing then
+      self.shinyAlpha = self.shinyAlpha + 128 * dt
+      if self.shinyAlpha > 128 then
+        self.shinyAlpha = 128
+        self.shinyIncreasing = false
+      end
+    else
+      self.shinyAlpha = self.shinyAlpha - 128 * dt
+      if self.shinyAlpha < 0 then
+        self.shinyAlpha = 0
+        self.shinyIncreasing = true
+      end
+    end
+  end
 end
 
 --[[
@@ -48,4 +67,10 @@ function Tile:render(x, y)
     love.graphics.setColor(255, 255, 255, 255)
     love.graphics.draw(gTextures['main'], gFrames['tiles'][self.color][self.variety],
         self.x + x, self.y + y)
+
+    --draw shiny
+    if self.shiny then
+      love.graphics.setColor(255, 255, 255, self.shinyAlpha)
+      love.graphics.rectangle('fill', self.x + x, self.y + y, 32, 32, 6)
+    end
 end
