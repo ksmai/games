a5daf818ec00714ca971689f130ecb4db52444eb
ksmai <kasingmai@gmail.com>
2018-04-15 18:15:43 +0800

    Add basic setup for space-invader/love2d

---
+++ space-invader/love2d/main.lua
@@ -0,0 +1,47 @@
+require 'src/Dependencies'
+
+function love.load()
+  love.graphics.setDefaultFilter('nearest', 'nearest')
+  love.window.setTitle('Space Invader')
+  push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
+    vsync = true,
+    resize = true,
+    fullscreen = false,
+  })
+  love.keyboard.keysPressed = {}
+end
+
+function love.keypressed(key)
+  if love.keyboard.isDown('escape') then
+    love.event.quit()
+  end
+
+  love.keyboard.keysPressed[key] = true
+end
+
+function love.keyboard.wasPressed(key)
+  return love.keyboard.keysPressed[key]
+end
+
+function love.resize(w, h)
+  push:resize(w, h)
+end
+
+function love.update(dt)
+  Timer.update(dt)
+  love.keyboard.keysPressed = {}
+end
+
+function love.draw()
+  push:start()
+  love.graphics.setFont(gFonts['small'])
+  love.graphics.setColor(0, 255, 0, 255)
+  love.graphics.printf(
+    'FPS: ' .. tostring(love.timer.getFPS()),
+    0,
+    0,
+    VIRTUAL_WIDTH,
+    'right'
+  )
+  push:finish()
+end
---
+++ space-invader/love2d/src/Dependencies.lua
@@ -0,0 +1,9 @@
+push = require 'lib/push'
+Class = require 'lib/class'
+Timer = require 'lib/knife.timer'
+Event = require 'lib/knife.event'
+
+VIRTUAL_WIDTH = 288
+VIRTUAL_HEIGHT = 216
+WINDOW_WIDTH = 1024
+WINDOW_HEIGHT = 768
