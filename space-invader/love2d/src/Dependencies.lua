5d29c6f16180695a3fc3f61dfb8a7d29d33c2c25
ksmai <kasingmai@gmail.com>
2018-04-22 19:05:51 +0800

    Add sounds and font files

--- space-invader/love2d/src/Dependencies.lua
+++ space-invader/love2d/src/Dependencies.lua
@@ -3,7 +3,23 @@ Class = require 'lib/class'
 Timer = require 'lib/knife.timer'
 Event = require 'lib/knife.event'
 
+require 'src/Util'
+
 VIRTUAL_WIDTH = 288
 VIRTUAL_HEIGHT = 216
 WINDOW_WIDTH = 1024
 WINDOW_HEIGHT = 768
+
+gFonts = {
+  small = love.graphics.newFont('font/font.ttf', 8),
+  medium = love.graphics.newFont('font/font.ttf', 8),
+  large = love.graphics.newFont('font/font.ttf', 16),
+}
+
+gSounds = {
+  explode = love.audio.newSource('sounds/explode.wav', 'static'),
+  shoot = love.audio.newSource('sounds/shoot.wav', 'static'),
+  shoot2 = love.audio.newSource('sounds/shoot2.wav', 'static'),
+  random = love.audio.newSource('sounds/random.wav', 'static'),
+  music = love.audio.newSource('sounds/music.wav', 'static'),
+}
