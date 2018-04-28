2d45ac867aa0df78262e6f7c2b4d7594fa8e0cdb
ksmai <kasingmai@gmail.com>
2018-04-28 12:13:19 +0800

    Add a probuilder prototype

---
+++ probuilder/unity/Pro Builder/Assets/Scripts/RestartOnFall.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartOnFall : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    Debug.Log(transform.position.y);
+    if (transform.position.y < -10) {
+      SceneManager.LoadScene("MainScene");
+    }
+	}
+}
---
+++ probuilder/unity/Pro Builder/Assets/Scripts/WinOnCollide.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WinOnCollide : MonoBehaviour {
+  public Text text;
+
+	// Use this for initialization
+	void Start () {
+    text.color = new Color(0, 0, 0, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    
+	}
+
+  void OnTriggerEnter() {
+    text.text = "YOU WIN!";
+    text.color = new Color(0, 0, 0, 1);
+  }
+}
